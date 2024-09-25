c0cfd50526ecaed9625369c067837531c8540039
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 17:17:57 +0300

    Minor page refactoring

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,14 +4,12 @@ import Link from "next/link";
 
 const IssuesPage = () => {
     return (
-        <div>
-            <h1>Issues Page</h1>
-            <Button>
+        <div className="space-y-4">
+            <h1 className="text-4xl font-bold">Issues Page</h1>
+            <Button size={"4"}>
                 <Link href="/issues/new">New Issue</Link>
             </Button>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto asperiores impedit tempora. Cumque dignissimos, dolore dolores dolorum earum, eos et excepturi illum ipsum numquam quas quasi rerum velit vitae.
-            </p>
+
 
         </div>
     );
