43f361d11b7e85f3b94f34f8034eda390e0e0650
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 14:42:57 +0300

    Add radix ui, and configure theme settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import {Button} from "@radix-ui/themes";
+
 export default function Home() {
   return (
     <div>
+        <Button size={"4"}>Click me</Button>
       This is my nextjs app
     </div>
   );
