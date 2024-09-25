36aaba81ec15e94d997857309760f358254d627f
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 06:04:53 +0300

    Add navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React from 'react';
+import Link from "next/link";
+import {FaBug} from "react-icons/fa6";
+import classnames from "classnames";
+import {usePathname} from "next/navigation";
+
+const NavBar = () => {
+    const pathname = usePathname();
+
+    const links = [
+        {label: "Dashboard", href: "/"},
+        {label: "Issues", href: "/issues"},
+        {label: "About", href: "/about"},
+    ]
+    return (
+        <nav className="
+            flex
+            space-x-6
+            border-b
+            h-14
+            items-center
+            px-5"
+        >
+            <Link href="/">
+                <FaBug size={32}/>
+            </Link>
+            <ul className="flex space-x-6">
+                {
+                    links.map(
+                        link =>
+                            <Link
+                                key={link.label}
+                                href={link.href}
+                                className={
+                                    classnames({
+                                        'text-slate-900': link.href === pathname,
+                                        'text-slate-500': link.href !== pathname,
+                                        'hover:text-slate-800 transition-colors': true
+                                    })
+                                }
+                            >
+                                {link.label}
+                            </Link>
+                    )
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IssuesPage = () => {
+    return (
+        <div>
+            <h1>Issues Page</h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto asperiores impedit tempora. Cumque dignissimos, dolore dolores dolorum earum, eos et excepturi illum ipsum numquam quas quasi rerum velit vitae.
+        </div>
+    );
+};
+
+export default IssuesPage;
