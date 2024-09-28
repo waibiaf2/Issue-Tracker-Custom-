58a6d2d7d3cd10b8d1f1b99c39e19d14070316c8
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-28 10:07:16 +0300

    update validations errors

--- app/validationsSchemas.ts
+++ app/validationsSchemas.ts
@@ -2,8 +2,8 @@ import {z} from "zod";
 
 export const createIssueSchema = z.object({
     title: z.string({message: "Title should be a string"})
-        .min(1)
+        .min(1,{message: "Title should be at least 1 character"})
         .max(255, {message: "Title should be between 1 and 255 characters"}),
-    description: z.string({message: "Description should be a string, with a minimum length of 1 characters"})
+    description: z.string({message: "Description should be a strings"})
         .min(1),
 })
